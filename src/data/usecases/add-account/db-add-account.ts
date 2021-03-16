3c1c0c96ae3c3cfedd510bb8d48c0d94442900df
Gibran Tavares <gibra.tavares@g4flex.com.br>
2021-03-16 10:31:28 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,16 +1,23 @@
 import { Encrypter } from '../../protocols/encrypter'
 import { AccountModel } from '../../../domain/models/account'
+import { AddAccountRepository } from '../../protocols/add-account-repository'
 import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
+  private readonly addAccountRepository: AddAccountRepository
 
-  constructor (encrypter: Encrypter) {
+  constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
     this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
   }
 
   async add (account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password)
+    const hashedPassword = await this.encrypter.encrypt(account.password)
+    await this.addAccountRepository.add({
+      ...account,
+      password: hashedPassword
+    })
     return new Promise((resolve) => {
       resolve({
         id: 1,
