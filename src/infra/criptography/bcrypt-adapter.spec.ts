a29a48cd60a17fa8f9f8e60d36039e5a605c2b52
Gibran Tavares <gibra.tavares@g4flex.com.br>
2021-03-18 21:35:10 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -10,4 +10,14 @@ describe('', () => {
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
+
+  test('should return a hash on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(async () => {
+      return Promise.resolve('hash_value')
+    })
+    const hashValue = await sut.encrypt('any_value')
+    expect(hashValue).toBe('hash_value')
+  })
 })
