a841e396b252c5f3767034e2598c6d7a827c21ee
Gibran <mac01gibran@gmail.com>
2021-03-28 19:34:05 -0300

    feat: ensure SignUp route returns 200 on success

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 
 import { bodyParser, cors } from '../middlewares'
+import setUpRoutes from './routes'
 
 export default (app: Express) => {
   app.use(bodyParser)
   app.use(cors)
+  setUpRoutes(app)
 }
