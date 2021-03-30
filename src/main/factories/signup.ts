bfa0bd1d916fed09ab6bb5a1766b7c8a986091b3
Gibran <mac01gibran@gmail.com>
2021-03-30 20:58:37 -0300

    feat: ensure handle controller method is called inside LogControllerDecorator

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,15 +1,17 @@
+import { LogControllerDecorator } from './../decorators/log'
 import { BcryptAdapter } from './../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from './../../infra/db/mongodb/account-repository/account'
 import { DbAddAccount } from './../../data/usecases/add-account/db-add-account'
 import { EmailValidatorAdapter } from './../../utils/email-validator-adapter'
 import { SignUpController } from './../../presentation/controllers/signup/sign-up-controller'
+import { Controller } from '../../presentation/protocols'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
-  return signUpController
+  return new LogControllerDecorator(signUpController)
 }
