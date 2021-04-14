0f01bd41c3fe3a44dca10dd5e83500cfbe7f7ce8
Gibran <mac01gibran@gmail.com>
2021-04-13 21:35:00 -0300

    refactor: move password comparation to the ValidationComposite

--- src/presentation/controllers/signup/sign-up-controller.spec.ts
+++ src/presentation/controllers/signup/sign-up-controller.spec.ts
@@ -87,20 +87,6 @@ describe('SingUp Controller', () => {
     expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
   })
 
-  test('should return 400 if password confirmation fails', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_invalid_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'invalid_password'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new InvalidParamError('passwordConfirmation')))
-  })
-
   test('should call EmailValidator with correct email', async () => {
     const { sut, emailValidatorStub } = makeSut()
     const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
