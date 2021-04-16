f99df14ee420ca478b9f170ec2526c288d1531b6
Gibran <mac01gibran@gmail.com>
2021-04-15 21:05:14 -0300

    refactor: move Validation to protocols folder

--- src/main/factories/signup/signup-validation.ts
+++ src/main/factories/signup/signup-validation.ts
@@ -2,7 +2,7 @@ import { EmailValidatorAdapter } from './../../../utils/email-validator-adapter'
 import { EmailValidation } from './../../../presentation/helpers/validators/email-validation'
 import { CompareFieldsValidation } from './../../../presentation/helpers/validators/compare-fields-validation'
 import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
-import { Validation } from '../../../presentation/helpers/validators/validation'
+import { Validation } from '../../../presentation/protocols/validation'
 import { ValidationComposite } from './../../../presentation/helpers/validators/validation-composite'
 
 export const makeSignUpValidation = (): ValidationComposite => {
