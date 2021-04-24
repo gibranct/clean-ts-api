a83b955857055c4991074e60bc663bed95326c3b
Gibran <mac01gibran@gmail.com>
2021-04-24 20:08:22 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,16 +2,24 @@ import { TokenGenerator } from './../../protocols/db/token-generator'
 import { HashComparer } from '../../protocols/cryptography/hash-comparer'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 import { Authentication, AuthenticationModel } from './../../../domain/usecases/authentication'
+import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repostiory'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepo: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
   private readonly tokenGenerator: TokenGenerator
+  private readonly updateAccessTokenRepo: UpdateAccessTokenRepository
 
-  constructor (loadAccountByEmailRepo: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator) {
+  constructor (
+    loadAccountByEmailRepo: LoadAccountByEmailRepository,
+    hashComparer: HashComparer,
+    tokenGenerator: TokenGenerator,
+    updateAccessTokenRepo: UpdateAccessTokenRepository
+  ) {
     this.loadAccountByEmailRepo = loadAccountByEmailRepo
     this.hashComparer = hashComparer
     this.tokenGenerator = tokenGenerator
+    this.updateAccessTokenRepo = updateAccessTokenRepo
   }
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
@@ -20,6 +28,7 @@ export class DbAuthentication implements Authentication {
       const hasCorrectPassword = await this.hashComparer.compare(authentication.password, accountModel.password)
       if (hasCorrectPassword) {
         const accessToken = await this.tokenGenerator.generate(accountModel.id)
+        await this.updateAccessTokenRepo.update(accountModel.id, accessToken)
         return accessToken
       }
     }
