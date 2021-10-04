f06c2febf2d4c532946d2ddd4f501537fceb66b5
Gibran <mac01gibran@gmail.com>
2021-10-03 22:22:58 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,7 +1,11 @@
+import { hash } from 'bcrypt'
+import { Collection } from 'mongodb'
 import request from 'supertest'
 
 import { mongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+
+let accountCollection: Collection
 
 describe('SignUp Routes', () => {
   beforeAll(async () => {
@@ -13,19 +17,49 @@ describe('SignUp Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await mongoHelper.getCollection('accounts')
+    accountCollection = await mongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
-  test('should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
+  describe('POST /signup', () => {
+    test('should return an account on success', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'name',
+          email: 'name@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('should return 200 on login', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
         name: 'name',
         email: 'name@gmail.com',
-        password: '123',
-        passwordConfirmation: '123'
+        password
       })
-      .expect(200)
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'name@gmail.com',
+          password: '123'
+        })
+        .expect(200)
+    })
+
+    test('should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'name@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
