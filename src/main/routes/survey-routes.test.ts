c4d42f8dbd7867833dcdf1438f1d44f7349b4cf7
Gibran <mac01gibran@gmail.com>
2021-10-23 10:32:45 -0300

    test: ensure add survey route returns 204 when valid accessToken is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,12 @@
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 
 import { mongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import env from '../config/env'
 
+let surveyCollection: Collection
 let accountCollection: Collection
 
 describe('Survey Routes', () => {
@@ -16,7 +19,9 @@ describe('Survey Routes', () => {
   })
 
   beforeEach(async () => {
-    accountCollection = await mongoHelper.getCollection('surveys')
+    surveyCollection = await mongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    accountCollection = await mongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -34,6 +39,31 @@ describe('Survey Routes', () => {
           ]
         })
         .expect(403)
+    })
+
+    test('should return 204 on add survey with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        role: 'admin'
+      })
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({ _id: id }, { $set: { accessToken } })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'any_question',
+          answers: [
+            {
+              answer: 'any_answer',
+              image: 'any_image'
+            }
+          ]
+        })
+        .expect(204)
     })
   })
 })
