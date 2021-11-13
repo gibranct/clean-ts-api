982b6941cb06df9e30945550013bcbbb69e2a453
Gibran <mac01gibran@gmail.com>
2021-11-13 15:23:28 -0300

    test: ensure LoadSurveys route works correctly

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -66,4 +66,54 @@ describe('Survey Routes', () => {
         .expect(204)
     })
   })
+
+  describe('GET /surveys', () => {
+    test('should return 403 on add survey without accessToken', async () => {
+      await request(app)
+        .get('/api/surveys')
+        .expect(403)
+    })
+
+    test('should return 204 when load survey is empty', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        role: 'admin'
+      })
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({ _id: id }, { $set: { accessToken } })
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send()
+        .expect(204)
+    })
+
+    test('should return 200 when load surveys is not empty', async () => {
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          answer: 'any_anwser',
+          image: 'any_image'
+        }],
+        date: new Date()
+      }])
+      const res = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        role: 'admin'
+      })
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({ _id: id }, { $set: { accessToken } })
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send()
+        .expect(200)
+    })
+  })
 })
