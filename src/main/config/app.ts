3b5996b43f10485a287f50e1dfe185bd7add6ba0
Gibran <mac01gibran@gmail.com>
2021-11-16 22:44:01 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 
 import setUpMiddlewares from './middlewares'
+import setupRoutes from './routes'
+import setupSwagger from './config-swagger'
 
 const app = express()
 
+setupSwagger(app)
 setUpMiddlewares(app)
+setupRoutes(app)
 
 export default app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,9 @@
+
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    version: '1.0.0',
+    description: 'A clean node api'
+  }
+}
